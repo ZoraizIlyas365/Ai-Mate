56ff17f3ce3d18c1eaaa2a0117ef8c0731b02b7d
Zoraiz Ilyas <zoraizilyas@gmail.com>
2026-04-21 20:25:44 +0500

    Updated Llama Model

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@ import Config from 'react-native-config';
 
 const env = {
   GROQ_API_KEY: Config.GROQ_API_KEY ?? '',
-  GROQ_MODEL: Config.GROQ_MODEL ?? 'llama-3.2-11b-vision-preview',
+  GROQ_MODEL: Config.GROQ_MODEL ?? 'meta-llama/llama-4-scout-17b-16e-instruct',
 };
 
 export default env;
